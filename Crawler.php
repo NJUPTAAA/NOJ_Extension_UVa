834f77edcc5439530e519d5a274b8134ce2eca2a
David Diao <daviddiaoyf@gmail.com>
2019-07-20 03:36:58 +0800

    supported cache

--- Crawler.php
+++ Crawler.php
@@ -10,8 +10,7 @@ use Exception;
 
 class Crawler extends CrawlerBase
 {
-    public $oid=null;
-    public $prefix="UVa";
+    public $oid = null;
     private $con;
     private $imgi;
     /**
@@ -19,21 +18,23 @@ class Crawler extends CrawlerBase
      *
      * @return Response
      */
-    public function __construct($conf)
+    public function start($conf)
     {
-        $action=isset($conf["action"])?$conf["action"]:'crawl_problem';
-        $con=isset($conf["con"])?$conf["con"]:'all';
-        $cached=isset($conf["cached"])?$conf["cached"]:false;
-        $this->oid=OJModel::oid('uva');
+        $action = isset($conf["action"]) ? $conf["action"] : 'crawl_problem';
+        $con = isset($conf["con"]) ? $conf["con"] : 'all';
+        $cached = isset($conf["cached"]) ? $conf["cached"] : false;
+        $this->oid = OJModel::oid('uva');
 
-        if(is_null($this->oid)) {
+        if (is_null($this->oid)) {
             throw new Exception("Online Judge Not Found");
         }
 
-        if ($action=='judge_level') {
+        $this->problemModel = new ProblemModel();
+
+        if ($action == 'judge_level') {
             $this->judge_level();
         } else {
-            $this->crawl($con);
+            $this->crawl($con, $action == 'update_problem', $cached);
         }
     }
 
@@ -42,53 +43,68 @@ class Crawler extends CrawlerBase
         // TODO
     }
 
-    public function crawl($con)
+    public function crawl($con, $incremental, $cached)
     {
-        $problemModel=new ProblemModel();
-        if ($con=='all') {
-            $res=Requests::get("https://uhunt.onlinejudge.org/api/p");
-            $result=json_decode($res->body, true);
-            $info=[];
-            for ($i=0; $i<count($result); ++$i) {
-                $info[$result[$i][1]]=[$result[$i][0], $result[$i][2], $result[$i][3], $result[$i][19]];
+        if ($cached) {
+            $res = file_get_contents(__DIR__ . '/problemset.problems');
+        } else {
+            $res = Requests::get("https://uhunt.onlinejudge.org/api/p", [], ['timeout' => 600]);
+            file_put_contents(__DIR__ . '/problemset.problems', $res = $res->body);
+        }
+        $result = json_decode($res, true);
+        if ($con == 'all') {
+            $info = [];
+            for ($i = 0; $i < count($result); ++$i) {
+                $info[$result[$i][1]] = [$result[$i][0], $result[$i][2], $result[$i][3], $result[$i][19]];
             }
             ksort($info);
-            foreach ($info as $key=>$value) {
-                $this->pro['pcode']='UVA'.$key;
-                $this->pro['OJ']=$this->oid;
-                $this->pro['contest_id']=null;
-                $this->pro['index_id']=$value[0];
-                $this->pro['origin']="https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=".$value[0];
-                $this->pro['title']=$value[1];
-                $this->pro['time_limit']=$value[3];
-                $this->pro['memory_limit']=131072; // Given in elder codes
-                $this->pro['solved_count']=$value[2];
-                $this->pro['input_type']='standard input';
-                $this->pro['output_type']='standard output';
-                $this->pro['description']="<a href=\"/external/gym/UVa{$key}.pdf\">[Attachment Link]</a>";
-                $this->pro['input']='';
-                $this->pro['output']='';
-                $this->pro['note']='';
-                $this->pro['sample']=[];
-                $this->pro['source']='Here';
-                $this->pro['file']=1;
-
-                $problem=$problemModel->pid($this->pro['pcode']);
-
-                if ($problem) {
-                    $problemModel->clearTags($problem);
-                    $new_pid=$this->update_problem($this->oid);
-                } else {
-                    $new_pid=$this->insert_problem($this->oid);
-                }
-
-                // $problemModel->addTags($new_pid, $tag); // not present
+            foreach ($info as $key => $value) {
+                $this->_crawl($key, $value);
             }
-            $this->data=array_keys($info);
         } else {
-            $pf=substr($con, 0, strlen($con)-2);
-            $res=Requests::get("https://uva.onlinejudge.org/external/$pf/p$con.pdf");
-            file_put_contents(base_path("public/external/gym/UVa$con.pdf"), $res->body);
+            for ($i = 0; $i < count($result); ++$i) {
+                if ($result[$i][1] == $con) {
+                    $this->_crawl($con, [$result[$i][0], $result[$i][2], $result[$i][3], $result[$i][19]]);
+                    break;
+                }
+            }
         }
+    }
+
+    private function _crawl($con, $info)
+    {
+        $this->pro['pcode'] = 'UVA' . $con;
+        $this->pro['OJ'] = $this->oid;
+        $this->pro['contest_id'] = null;
+        $this->pro['index_id'] = $info[0];
+        $this->pro['origin'] = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=" . $info[0];
+        $this->pro['title'] = $info[1];
+        $this->pro['time_limit'] = $info[3];
+        $this->pro['memory_limit'] = 131072; // Given in elder codes
+        $this->pro['solved_count'] = $info[2];
+        $this->pro['input_type'] = 'standard input';
+        $this->pro['output_type'] = 'standard output';
+        $this->pro['description'] = "<a href=\"/external/gym/UVa{$con}.pdf\">[Attachment Link]</a>";
+        $this->pro['input'] = '';
+        $this->pro['output'] = '';
+        $this->pro['note'] = '';
+        $this->pro['sample'] = [];
+        $this->pro['source'] = 'Here';
+        $this->pro['file'] = 1;
+
+        $problem = $this->problemModel->pid($this->pro['pcode']);
+
+        if ($problem) {
+            $this->problemModel->clearTags($problem);
+            $new_pid = $this->updateProblem($this->oid);
+        } else {
+            $new_pid = $this->insertProblem($this->oid);
+        }
+
+        // $this->problemModel->addTags($new_pid, $tag); // not present
+
+        $pf = substr($con, 0, strlen($con) - 2);
+        $res = Requests::get("https://uva.onlinejudge.org/external/$pf/p$con.pdf");
+        file_put_contents(base_path("public/external/gym/UVa$con.pdf"), $res->body);
     }
 }
